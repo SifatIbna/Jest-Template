aaac3811d61e31d29ef36fb8ba75efe2aafbed86
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-21 20:43:12 +0600

    reformatting codes

--- src/tests/utils/util.test.tsx
+++ src/tests/utils/util.test.tsx
@@ -1,105 +1,92 @@
-import * as CustomError from '../../utils/CustomError';
-import { Utils } from '../../utils/url'
+import * as CustomError from "../../utils/CustomError";
+import { Utils } from "../../utils/url";
 
-// const foo = {CustomError}
+//Consts
+const parsedUrl = Utils.parseUrl("http://localhost:8080/login?username=nestgo");
 
-// jest.mock('../../utils/CustomError',()=>{
-//     // return {
-//     //     CustomError: return jest.fn().mockImplementation(()=>{
-//     //         // return {
-//     //         //     mockConstructor: jest.fn(),
-//     //         //     getData : ()=>{
-//     //         //         return {
-//     //         //             "status":404,
-//     //         //             "statusText":"hello world"
-//     //         //         }
-//     //         //     }
-//     //         // }
-//     //         return jest.fn()
-//     //     })
-//     // }
-//     return jest.fn().mockImplementation(()=>{
-//         return {getData : jest.fn()}
-//     })
-// })
-
-
-
-//
-
-describe('Utils test suite', () => {
-
-    test('first test', () => {
-        const result = Utils.toUpperCase('abc');
-        expect(result).toBe('ABC');
+describe("Utils test suite", () => {
+  describe("Utils", () => {
+    test("first test", () => {
+      const result = Utils.toUpperCase("abc");
+      expect(result).toBe("ABC");
     });
 
-    test('parse simple URL', () => {
-        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
-        expect(parsedUrl.href).toBe('http://localhost:8080/login');
-        expect(parsedUrl.port).toBe('8080');
-        expect(parsedUrl.protocol).toBe('http:');
-        expect(parsedUrl.query).toEqual({});
+    it("given an url,retruns the href", () => {
+      const href = parsedUrl.href;
+      const expectedHref = "http://localhost:8080/login?username=nestgo";
+
+      expect(href).toBe(expectedHref);
     });
 
-    test('parse URL with query', () => {
-        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
-        const expectedQuery = {
-            user: 'user',
-            password: 'pass'
-        }
-        expect(parsedUrl.query).toEqual(expectedQuery);
-        expect(expectedQuery).toBe(expectedQuery);
-    })
-
-    test('test invalid URL', () => {
-        function expectError() {
-            Utils.parseUrl('')
-        }
-        expect(expectError).toThrow();
+    it("given a url, return the port", () => {
+      const port = parsedUrl.port;
+      expect(parsedUrl.port).toBe("8080");
     });
 
-    test.skip('test invalid URL with arrow function', () => {
-        expect(() => {
-            Utils.parseUrl('')
-        }).toThrow('Empty url');
+    it("given a url, return the protocol", () => {
+      const protocol = parsedUrl.protocol;
+      expect(parsedUrl.protocol).toBe("http:");
     });
 
-    test('test invalid URL with try catch', () => {
-        // jest.spyOn(foo,'CustomError')
-        try {
-            Utils.parseUrl('');
-        } catch (error) {
-            console.log(error)
-            expect(error).toBeInstanceOf(CustomError.CustomError)
-            // expect(CustomError.CustomError).toHaveBeenCalled();
-        }
+    it("given a url, return the query", () => {
+      const query = parsedUrl.query;
+      const expectedObj = {
+        username: "nestgo",
+      };
+      expect(query).toEqual(expectedObj); // deep equality
     });
 
-    test("checking constructor",()=>{
-        const customErrorConstructorMock = jest.spyOn(CustomError,'CustomError')
-        const testData = {
-            statusCode:404,
-            statusText:"heeloo"
-        }
+    it("given an url, thorws error", () => {
+      function expectError() {
+        Utils.parseUrl("");
+      }
+      expect(expectError).toThrow();
+    });
 
-        new CustomError.CustomError(testData)
-        expect(customErrorConstructorMock).toHaveBeenCalledWith(testData)
-    })
+    it.skip("test invalid URL with arrow function", () => {
+      expect(() => {
+        Utils.parseUrl("");
+      }).toThrow("Empty url");
+    });
 
-    test('another test',()=>{
+    it("given an url, throws Custom Error", () => {
+      try {
+        Utils.parseUrl("");
+      } catch (error) {
+        console.log(error);
+        expect(error).toBeInstanceOf(CustomError.CustomError);
+      }
+    });
+  });
+  describe("CustomError", () => {
+    it("given invalid url, throws custom error with proper parameters", () => {
+      const customErrorConstructorMock = jest.spyOn(CustomError, "CustomError");
+      const testData = {
+        statusCode: 404,
+        statusText: "heeloo",
+      };
 
-        const customErrorMock = jest.spyOn(CustomError.CustomError.prototype,'getData').mockImplementation(()=>{return{
-            statusCode:404,
-            statusText:"return"
-        }})
+      new CustomError.CustomError(testData);
+      expect(customErrorConstructorMock).toHaveBeenCalledWith(testData);
+    });
 
-        const errorObj = new CustomError.CustomError({
-            statusCode:303,
-            statusText:"text"
-        })
+    it("given invalid url throws custom error, gets the proper data from getData method", () => {
+      const customErrorMock = jest
+        .spyOn(CustomError.CustomError.prototype, "getData")
+        .mockImplementation(() => {
+          return {
+            statusCode: 404,
+            statusText: "return",
+          };
+        });
 
-        errorObj.getData()
-        expect(customErrorMock).toHaveBeenCalled();
-    })
+      const errorObj = new CustomError.CustomError({
+        statusCode: 303,
+        statusText: "text",
+      });
+
+      errorObj.getData();
+      expect(customErrorMock).toHaveBeenCalled();
+    });
+  });
 });
