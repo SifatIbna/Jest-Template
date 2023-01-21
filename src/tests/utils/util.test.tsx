2fb5a3f3e98bc153c2b376196c75ef0befcf8ad4
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-21 18:01:11 +0600

    added a util function & added some tests

---
+++ src/tests/utils/util.test.tsx
@@ -0,0 +1,28 @@
+import { Utils } from '../../utils/url'
+
+
+describe('Utils test suite', () => {
+
+    test('first test', () => {
+        const result = Utils.toUpperCase('abc');
+        expect(result).toBe('ABC');
+    });
+
+    test('parse simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    });
+
+    test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(expectedQuery).toBe(expectedQuery);
+    })
+});
